61b289b7e44461cd4fbc84093ee5359ceef4960f
Bartłomiej Szymocha <bartlomiejszymocha@MacBookPro.toya>
2025-09-27 15:50:27 +0200

    Change 'zyskasz' to 'odzyskasz' in subtitle for better messaging

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -59,7 +59,7 @@ export const LoginScreen: React.FC<LoginScreenProps> = ({ onSignIn }) => {
                 <div className="text-center">
                     <div className="text-7xl mb-6 text-white animate-pulse">🚀</div>
                     <h1 className="text-4xl font-bold text-white mb-3">Witaj w Energy Playbook!</h1>
-                    <p className="text-lg text-gray-300 mb-8">Proste i sprawdzone narzędzie, dzięki któremu zyskasz 2h produktywności dziennie. Przejmij kontrolę nad swoją energią już dziś!</p>
+                    <p className="text-lg text-gray-300 mb-8">Proste i sprawdzone narzędzie, dzięki któremu odzyskasz 2h produktywności dziennie. Przejmij kontrolę nad swoją energią już dziś!</p>
                     
                     <button
                         onClick={handleSignInClick}
